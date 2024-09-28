776b620ac6947a5f68864a0540dec80b93b787e7
Dr. Ayin <angrysqueak225@gmail.com>
2024-09-29 02:59:24 +0700

    исисит

--- build/gtests/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/Users/user/Desktop/2laba/build/gtests/tests[1]_tests.cmake")
-  include("C:/Users/user/Desktop/2laba/build/gtests/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
